15217fa92472b22e1ff4b916fc6e05fed584be65
daedalus-s <sreenikethaathreya@gmail.com>
2024-12-08 20:32:54 -0500

    commit2

--- get_realm.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-site_url = "https://humcodetechnologies143.sharepoint.com/sites/Sharepoint-Bedrock-Test"
-response = requests.get(f"{site_url}/_vti_bin/client.svc", 
-                       headers={"Authorization": "Bearer"}, 
-                       verify=True)
-www_authenticate = response.headers['WWW-Authenticate']
-realm = www_authenticate.split('Bearer realm="')[1].split('"')[0]
-print(f"Your realm ID is: {realm}")
